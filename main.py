e83018e501cd73ac5c38fc6eb1d2715d3d636c85
sau2som <sau2somnathe@gmail.com>
2024-11-12 19:56:02 +0530

    this is initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+if__name__=='__main__':
+print('this is main code')
+val1=int(input('enter first no of your choice :=')
+val2=int(input('enter second no of your choice :=')
+ op=input('which math operation you want to carry, add,sub,mult,div')
+
+
+         if op.lower() =='add':
+          a1=val1+val2
+          print(f'addition={a1}')
+          elif op.lower()=='sub':
+          a2=val1-val2
+          print(f'substraction={a2}')
+          elif op.lower=='mult':
+          a3=val1*val3
+          print(f'multiplication={a3}')
+          elif op.lower=='div':
+          a4=val1/val2
+          print(f'division={a4}')
+          else:
+          print('please enter right operation')
