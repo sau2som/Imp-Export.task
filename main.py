733db1efcbd63f4ee0e1bbd5fd0b41272fd3f766
sau2som <sau2somnathe@gmail.com>
2024-11-12 20:24:25 +0530

    new commit

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ op=input('which math operation you want to carry, add,sub,mult,div :')
 if op.lower() =='add':
     a1=addvalue(val1,val2)
     print(f'addition={a1}')
-elif op.lower()=='sub':
+elif op.lower() =='sub':
     a2=subvalue(val1,val2)
     print(f'substraction={a2}')
-elif op.lower=='mult':
+elif op.lower() =='mult':
     a3=multvalue(val1,val2)
     print(f'multiplication={a3}')
-elif op.lower=='div':
+elif op.lower() =='div':
     a4=divvalue(val1,val2)
     print(f'division={a4}')
 else:
